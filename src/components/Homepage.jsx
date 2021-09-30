a7daba3656bc62b4041f88e9b0dc9b03f9a40908
Pablo Costanzo <costanzopa@gmail.com>
2021-09-29 23:13:48 -0300

    Adding Cryptocurrencies

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -6,6 +6,8 @@ import { useGetCryptosQuery } from '../services/cryptoApi';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
+import { Cryptocurrencies, News } from '.';
+
 const { Title } = Typography;
 
 const Homepage = (props) => {
@@ -53,6 +55,24 @@ const Homepage = (props) => {
           />
         </Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top 10 Cryptos In The World
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/cryptocurrencies">Show more</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Latest Crypto News
+        </Title>
+        <Title level={3}>
+          <Link to="/news">Show more</Link>
+        </Title>
+      </div>
+      <News simplified />
     </>
   );
 };
