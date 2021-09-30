906c473548d20886df475adf1f9553aa14c3513b
Pablo Costanzo <costanzopa@gmail.com>
2021-09-30 16:29:29 -0300

    Adding Loader Spinner

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { Spin } from 'antd';
 
-const Loader = (props) => {
-  return <div>Loading...</div>;
-};
+const Loader = () => (
+  <div className="loader">
+    <Spin />
+  </div>
+);
+
 export default Loader;
